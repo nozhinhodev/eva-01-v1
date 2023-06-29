48891622da20f8324fd7c9e14f791c20cf3e76eb
Fathulla <fathullarasidov239@gmail.com>
2023-06-30 01:06:21 +0500

    init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import {Route, Routes} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
 
+
+      <Routes>
+        <Route index element='/'/>
+      </Routes>
     </div>
   );
 }
